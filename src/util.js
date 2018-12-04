c237d25afa3d64d59d3d5bae4456d7388cdededf
Richik SC <sc.richik@gmail.com>
2018-12-03 21:52:31 -0600

    Fixed file reading - relative to parent module

--- src/util.js
+++ src/util.js
@@ -2,7 +2,18 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = {
+  /**
+   * Reads input from input.txt in current folder using UTF-8 encoding.
+   * Returns file contents as a string.
+   *
+   * @returns {string} input.txt's contents as a string
+   */
   input() {
-    return fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'UTF-8');
+    return fs.readFileSync(
+      path.resolve(
+        path.dirname(module.parent.filename), 'input.txt'
+      ),
+      'UTF-8'
+    );
   }
 }
